5e0572ecd3db14d6eadb942014ba6329c4318614
zuzannabartczak <125361255+zuzannabartczak@users.noreply.github.com>
2023-10-30 22:04:05 +0100

    models and v1 of R code

---
+++ jsonl_to_csv.py
@@ -0,0 +1,18 @@
+import json
+import csv
+
+# Open the jsonl file for reading and the csv file for writing
+with open('data_json/SubtaskA/subtaskA_train_monolingual.jsonl', 'r') as json_file, open('data_csv/subtaskA_train_monolingual.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    # Create a csv writer object
+    writer = csv.writer(csv_file)
+
+    # Write the header row to the csv file
+    writer.writerow(['text', 'label', 'model', 'source', 'id'])
+
+    # Loop through each line in the jsonl file
+    for line in json_file:
+        # Load the json data from the line
+        data = json.loads(line)
+
+        # Write the data to the csv file
+        writer.writerow([data['text'], data['label'], data['model'], data['source'], data['id']])
