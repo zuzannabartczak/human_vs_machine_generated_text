8968f49b9e44a2e69c3645c34fbc773ba24a9045
zuzannabartczak <125361255+zuzannabartczak@users.noreply.github.com>
2023-10-31 18:17:16 +0100

    Revert "data for plots"
    This reverts commit 62e8acbee27eecbbcc6d05bd96d042f01c4fa402.

--- Project_Code.R
+++ Project_Code.R
@@ -1,164 +1,82 @@
 library(ggplot2)
 library(dplyr)
-library(nortest)
-library(jsonlite)
-library(tidyverse)
 
+taskAmono <- read.csv("data_csv/subtaskA_train_monolingual.csv", header = TRUE)
 
-#Load data
-
-train_data <- read.csv("data_csv/subtaskA_train_monolingual.csv", header = TRUE)
-dev_data <- read.csv("data_csv/subtaskA_dev_monolingual.csv", header = TRUE)
-
-sgd_lc <- read.csv("SGD_outputs/learning_curve.csv", header = TRUE)
-
-sgd_cm <- read.csv("SGD_outputs/confusion_matrix.csv", header = TRUE)
-rnn_cm <- read.csv("RNN_outputs/confusion_matrix.csv", header = TRUE)
-
-sgd_report <- read.csv("SGD_outputs/classification_report.csv", header = TRUE)
-rnn_report <- read.csv("RNN_outputs/classification_report.csv", header = TRUE)
-
-sgd_weights <- read.csv("SGD_outputs/weights.csv", header = TRUE)
-rnn_weights <- read.csv("RNN_outputs/weights.csv", header = TRUE)
-
-sgd_top <- read.csv("SGD_outputs/top_bottom_words.csv", header = TRUE)
-rnn_top <- read.csv("RNN_outputs/top_bottom_words.csv", header = TRUE)
-
-sgd_roc <- read.csv("SGD_outputs/ROC.csv", header = TRUE)
-rnn_roc <- read.csv("RNN_outputs/ROC.csv", header = TRUE)
-
-lines <- readLines("SGD_outputs/dev_predictions.jsonl")
-lines <- lapply(lines, fromJSON)
-sgd_pred <- bind_rows(lines)
-
-lines2 <- readLines("RNN_outputs/dev_predictions.jsonl")
-lines2 <- lapply(lines2, fromJSON)
-rnn_pred <- bind_rows(lines2)
-
-
+# BEGIN: eda_visualizations
 
 # Distribution of human vs. machine-generated texts
+ggplot(taskAmono, aes(x = label)) +
+    geom_bar() +
+    ggtitle("Distribution of Human vs. Machine-Generated Texts in Monolingual Dataset")
 
-# Remove rows with NA in 'model' or 'label' column
-train_data <- train_data %>% drop_na(model, label)
+ggplot(taskAmulti, aes(x = label)) +
+    geom_bar() +
+    ggtitle("Distribution of Human vs. Machine-Generated Texts in Multilingual Dataset")
 
-ggplot(train_data, aes(x = model, fill = model)) +
+# Distribution across different languages and domains
+ggplot(taskB, aes(x = model, fill = label)) +
     geom_bar(position = "dodge") +
-    ggtitle("Distribution of Human vs. Machine-Generated Texts")
-
-ggplot(train_data, aes(x = model, fill = source)) +
-    geom_bar(position = "dodge") +
-    ggtitle("Distribution of domains")
+    ggtitle("Distribution of Human vs. Machine-Generated Texts Across Languages in Subtask B")
 
 
-# Calculate text length
-train_data$length <- str_length(train_data$text)
+# Create a data frame with the weights
+weights_df <- read.csv("weights.csv", header = TRUE)
 
-# Define the percentiles to remove from each side
-lower_percentile <- 0.10  # Remove bottom 10%
-upper_percentile <- 0.90  # Remove top 10%
-
-# Calculate the corresponding text length for each percentile
-lower_limit <- quantile(train_data$length, lower_percentile)
-upper_limit <- quantile(train_data$length, upper_percentile)
-
-# Filter out texts outside the desired range
-length_filtered <- train_data %>% filter(length >= lower_limit & length <= upper_limit)
-
-# Plot the distribution of text length
-ggplot(length_filtered, aes(x = length)) +
-    geom_histogram(binwidth = 5, fill = "lightblue", color = "lightblue") +
-    labs(title = "Distribution of Text Length", x = "Text Length", y = "Frequency")
+# Plot the weights using ggplot2
+ggplot(weights_df, aes(x = 1:length(weights), y = weights)) +
+    geom_line() +
+    xlab("Value") +
+    ylab("Weights") +
+    theme_minimal()
 
 
-
-labels <- train_data$label
-labels <- as.numeric(labels)
-
-# Test for normality using the Anderson-Darling test
-ad_test <- ad.test(labels)
-
-# Print the test statistic and p-value
-cat("Anderson-Darling test statistic:", ad_test$statistic, "\n")
-cat("p-value:", ad_test$p.value, "\n")
-
-# Interpret the results
-if (ad_test$p.value > 0.05) {
-    cat("The data is normally distributed.\n")
-} else {
-    cat("The data is not normally distributed.\n")
-}
-
-
-# Print the mean and standard deviation for the SGD model
-cat("SGD model:\n")
-cat("Mean:", sgd_lc$train_scores_mean, "\n")
-cat("Standard deviation:", sgd_lc$train_scores_std, "\n")
-
-# Print the mean and standard deviation for the RNN model
-cat("RNN model:\n")
-cat("Mean:", rnn_mean, "\n")
-cat("Standard deviation:", rnn_std, "\n")
-
-
-
-# Test for statistical significance using the Mann-Whitney U test (between training and test or sgd and rnn??)
-wilcox_test <- wilcox.test(rnn_data, sgd_data)
-
-# Print the test statistic and p-value
-cat("Mann-Whitney U test statistic:", wilcox_test$statistic, "\n")
-cat("p-value:", wilcox_test$p.value, "\n")
-
-# Interpret the results
-if (wilcox_test$p.value > 0.05) {
-    cat("There is no significant difference between the RNN and SGD models.\n")
-} else {
-    cat("There is a significant difference between the RNN and SGD models.\n")
-}
-
-
-names(sgd_lc) <- c("TrainSize", "TrainScore", "TestScore", "TrainStdDev", "TestStdDev")
+# Load the learning curve data
+learning_curve <- read.csv("learning_curve.csv")
 
 # Plot the learning curve
-ggplot(sgd_lc, aes(TrainSize)) +
-    geom_line(aes(y = TrainScore), color = "darkgreen") +
-    geom_line(aes(y = TestScore), color = "purple") +
-    geom_ribbon(aes(ymin = TrainScore - TrainStdDev, ymax = TrainScore + TrainStdDev), alpha = 0.1, fill = "darkgreen") +
-    geom_ribbon(aes(ymin = TestScore - TestStdDev, ymax = TestScore + TestStdDev), alpha = 0.1, fill = "purple") +
-    labs(title = "Learning Curve", x = "Training Size", y = "Score") +
-    scale_x_continuous(expand = c(0, 0)) +
-    scale_y_continuous(expand = c(0, 0)) +
+ggplot(learning_curve, aes(x = train_sizes)) +
+    geom_line(aes(y = train_scores_mean), color = "blue", size = 1) +
+    geom_line(aes(y = test_scores_mean), color = "red", size = 1) +
+    geom_ribbon(aes(ymin = train_scores_mean - train_scores_std / 2,
+                    ymax = train_scores_mean + train_scores_std / 2),
+                fill = "blue", alpha = 0.2) +
+    geom_ribbon(aes(ymin = test_scores_mean - test_scores_std / 2,
+                    ymax = test_scores_mean + test_scores_std / 2),
+                fill = "red", alpha = 0.2) +
+    labs(x = "Training examples", y = "Score", color = "Legend") +
+    scale_color_manual(values = c("blue", "red"), labels = c("Training score", "Cross-validation score")) +
+    theme_minimal()
+
+# Define the confusion matrix
+confusion_matrix <- matrix(c(12198, 298, 7590, 3866), nrow = 2, byrow = TRUE)
+
+# Convert the confusion matrix to a data frame
+confusion_matrix_df <- data.frame(
+    actual = rep(c("Positive", "Negative"), each = 2),
+    predicted = rep(c("Positive", "Negative"), times = 2),
+    count = as.vector(confusion_matrix)
+)
+
+# Plot the confusion matrix
+ggplot(confusion_matrix_df, aes(x = actual, y = predicted, fill = count)) +
+    geom_tile() +
+    geom_text(aes(label = count)) +
+    scale_fill_gradient(low = "white", high = "blue") +
+    labs(x = "Actual", y = "Predicted", fill = "Count") +
     theme_minimal()
 
 
 
-# Plot weights
-ggplot(sgd_weights, aes(x = word, y = weight)) +
-    geom_bar(stat = 'identity') +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    labs(title = "Feature Weights", x = "Feature", y = "Weight")
 
+# Read the learning curve data from the csv file
+learning_curve_df <- read.csv("RNN_outputs/learning_curve.csv")
 
-# Plot the feature importance
-ggplot(df, aes(x = reorder(Feature, Importance), y = Importance)) +
-    geom_bar(stat = 'identity') +
-    coord_flip() +
-    labs(title = "Feature Importance", x = "Feature", y = "Importance")
-
-# Define the confusion matrix
-cm1 <- matrix(c(12198, 298, 7590, 3866), nrow = 2, byrow = TRUE)
-
-# Convert the confusion matrix to a data frame
-cm_sgd <- data.frame(
-    actual = rep(c("Positive", "Negative"), each = 2),
-    predicted = rep(c("Positive", "Negative"), times = 2),
-    count = as.vector(cm1)
-)
-
-# Plot the confusion matrix
-ggplot(cm_sgd, aes(x = actual, y = predicted, fill = count)) +
-    geom_tile() +
-    geom_text(aes(label = count)) +
-    scale_fill_gradient(low = "lightgrey", high = "purple") +
-    labs(x = "Actual", y = "Predicted", fill = "Count") +
+# Plot the learning curve
+ggplot(learning_curve_df, aes(x = Training.Sizes)) +
+    geom_line(aes(y = Training.Scores.Mean), color = "red") +
+    geom_line(aes(y = Validation.Scores.Mean), color = "green") +
+    geom_ribbon(aes(ymin = Training.Scores.Mean - Training.Scores.Std, ymax = Training.Scores.Mean + Training.Scores.Std), alpha = 0.1, fill = "red") +
+    geom_ribbon(aes(ymin = Validation.Scores.Mean - Validation.Scores.Std, ymax = Validation.Scores.Mean + Validation.Scores.Std), alpha = 0.1, fill = "green") +
+    labs(title = "Learning Curve", x = "Training Examples", y = "Score") +
     theme_minimal()
